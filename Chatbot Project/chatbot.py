2ceddfd23a03c88384ae8d06f309e087b743148e
Rashi-Salaria <108417578+Rashi-Salaria@users.noreply.github.com>
2022-08-08 21:38:04 +0530

    Add files via upload
    !st commit

---
+++ Chatbot Project/chatbot.py
@@ -0,0 +1,45 @@
+company_name=input("Enter the company Name:-")
+if company_name =="SAMSUNG":
+	print("model name = samsung galaxy f41")
+	print("memory storage = 64GB")
+	print("operating system = android")
+	print("camera = 64mega pixels")
+	print("colour = fusion green")
+	print("cellular technoloy = 4G")
+	print("model year = 2021")
+else:
+    print("Not Exist")
+
+
+brand_name=input("enter the brand name:-")
+if brand_name=="LENOVO":
+	print("series = ideapad 3 151ML05")
+	print("screen size =15.6 inches")
+	print("operating system = window11 home")
+	print("cpu manufacturer = intel")
+	print("special feature = thin")
+	print("colour = platinum grey")
+else:
+       print("Not Exist")
+
+
+
+
+
+
+brand_name=input("Enter the brand Name:-")
+if brand_name=="DELL":
+	print("series = optiplex-cr")
+	print("ram memory = 4GB")
+	print("operating system = window10 pro")
+	print("cpu manufacturer = intel")
+	print("screen size = 17Inches")
+else:
+      print("Not Exist")
+
+
+
+
+
+
+
